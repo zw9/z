5abcce2cfbd15be68117b4d9d8845eacc0fe7984
atang148 <atang148@pm.me>
2019-09-15 02:55:12 -0700

    aa

---
+++ .ipynb_checkpoints/nguoiviet-webscrape-checkpoint.py
@@ -0,0 +1,57 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# In[2]:
+
+
+def fetch_fnc(url,cl):
+    import re
+    import io
+    fn= re.sub('[^A-Za-z0-9]+', '', url.split("/")[-1] ) + ".txt"
+    fn
+    encoding = 'utf-8'
+    f = io.open(fn, "w+",encoding='utf-8')
+    page = requests.get(url)
+    page.status_code
+    soup = BeautifulSoup(page.content, 'html.parser')
+    ctl01_PageHead=soup.title
+    print("## " + ctl01_PageHead.get_text() + "  ")
+    f.write("## " + ctl01_PageHead.get_text() + "  ")
+    TBLRoll=soup.find_all( class_=cl)
+    for each in TBLRoll:
+        if each.get_text():
+            print("\n\n" + each.get_text().lstrip().rstrip().replace('\n', ' ') +"  ")
+            f.write("\n\n" + each.get_text().lstrip().rstrip().replace('\n', ' ') +"  ")
+    f.close()
+
+
+# In[3]:
+
+
+def fetch_option(url):
+    page = requests.get(url)
+    page.status_code
+    soup = BeautifulSoup(page.content, 'html.parser')
+    ctl01_PageHead=soup.title
+    print("## " + ctl01_PageHead.get_text() +"  ")
+    TBLRoll=soup.find( class_='col2Div').find_all('option')
+    cl='TBLRoll'
+    for each in TBLRoll:
+        if (each.text):
+            print(  "fetch_fnc(\"https://raovat.nguoi-viet.com" + each['value'] + "\")")
+            url_c="https://raovat.nguoi-viet.com" + each['value'] 
+            fetch_fnc(url_c,cl)
+
+
+# In[4]:
+
+
+fetch_option("https://raovat.nguoi-viet.com/classified/dam-bop-thu-gian-massage-relax-browse-123.aspx")
+
---
+++ web_fetch_def.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+def fetch_fnc(url,cl):
+    import re
+    import io
+    fn="/home/b4gone/Dropbox/txt/" + re.sub('[^A-Za-z0-9]+', '', url.split("/")[-1]) + ".txt"
+    fn
+    encoding = 'utf-8'
+    f = io.open(fn, "w+",encoding='utf-8')
+    page = requests.get(url)
+    page.status_code
+    soup = BeautifulSoup(page.content, 'html.parser')
+    f.write(unicode(url))
+    TBLRoll=soup.find_all(class_=cl)
+    for each in TBLRoll:
+        if each.get_text():
+            f.write( '' +  each.get_text().rstrip().replace('\n\n', ' '))
+    f.close()
+
