0f0028d4bb26caf75d7848fde95805c0d23604fa
zthequietone <atang148@pm.me>
2019-09-19 07:40:55 -0700

    a

--- weblist_inc_mk.py
+++ weblist_inc_mk.py
@@ -1,6 +1,4 @@
-#!//anaconda3/bin/python3
-
-##!/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
@@ -14,32 +12,30 @@ import re
 import io
 import glob
 
-#!pwd
+# !pwd
 
-fn="weblist_inc.md"
+fn = "weblist_inc.md"
 print(fn)
 encoding = 'utf-8'
 
-g = io.open(fn, "w",encoding='utf-8')
+g = io.open(fn, "w", encoding='utf-8')
 
-
-fnhtml=  "weblist_inc.html"
+fnhtml = "weblist_inc.html"
 print(fnhtml)
 encoding = 'utf-8'
 
-gfnhtml = io.open(fnhtml, "w",encoding='utf-8')
+gfnhtml = io.open(fnhtml, "w", encoding='utf-8')
 
-fnmd1 = glob.glob('./**/*.htm*',recursive=True)
-fnmd1.extend ( glob.glob('./**/*.txt',recursive=True))
-fnmd1.extend ( glob.glob('./**/*.md',recursive=True))
-#fnmd1.extend ( glob.glob('./**/*.ipynb',recursive=True))
-#display(fnmd)
+fnmd1 = glob.glob('./**/*.htm*', recursive=True)
+fnmd1.extend(glob.glob('./**/*.txt', recursive=True))
+fnmd1.extend(glob.glob('./**/*.md', recursive=True))
+# fnmd1.extend ( glob.glob('./**/*.ipynb',recursive=True))
+# display(fnmd)
 
 for f in fnmd1:
-    #print ( '#### [' + f + ']' + '(' + f +')' + "\n")
-    g.write( '#### [' + f + ']' + '(' + f +')' + "\n")
-    gfnhtml.write( '<a href="' + f + '">' + f +'</a><br>' + "\n")
+    # print ( '#### [' + f + ']' + '(' + f +')' + "\n")
+    g.write('#### [' + f + ']' + '(' + f + ')' + "\n")
+    gfnhtml.write('<a href="' + f + '">' + f + '</a><br>' + "\n")
 
 g.close()
 gfnhtml.close()
-        
