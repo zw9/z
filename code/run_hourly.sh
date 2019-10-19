9636fa8d6694095c88d02f3ad4dd927ca42f8133
zthequietone <atang148@pm.me>
2019-10-18 21:22:58 -0700

    a

--- code/run_hourly.sh
+++
@@ -1,9 +0,0 @@
-#
-
-find /Volumes/ThuDuc/z/ -name "**.txt" -size -2 -delete
-find /Volumes/ThuDuc/z -empty  -delete
-find /Volumes/ThuDuc/z -empty -type d -delete
-rm /Volumes/ThuDuc/z/rpt/*.html
-rm /Volumes/ThuDuc/z/rptvi/*.html
-python3 ~/Dropbox/z/code/filelist_mk.py
-python3 ~/Dropbox/z/code/filelistbyext.py
